cdc067d9b8edbb568ca561a724aa77709952394b
lhdecampos <luyzenhiyk@gmail.com>
2026-03-20 20:18:50 -0300

    feat: create DefaultInput component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { CountDown } from "./components/CountDown";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import { DefaultInput } from "./components/DefaultInput";
 
 export function App() {
   return (
@@ -19,6 +20,27 @@ export function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <DefaultInput id="task" type="text" />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
